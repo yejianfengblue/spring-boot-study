661cfb45cef66563f51255b5113d28b1174284f3
Jian Feng Ye <yejianfengblue@163.com>
2019-12-06 20:23:46 +0800

    Initial Spring Boot project with dependencies: - web - devtools - lombok

---
+++ src/main/java/com/yejianfengblue/spring/boot/SpringBootStudyApplication.java
@@ -0,0 +1,16 @@
+package com.yejianfengblue.spring.boot;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringBootStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStudyApplication.class, args);
+    }
+}
