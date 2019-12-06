527b721d7c21d3dc60846ce7a1cb5f2d91aa0434
Jian Feng Ye <yejianfengblue@163.com>
2019-12-06 22:25:18 +0800

    Use net.ttddyy:datasource-proxy to intercept SQL to log beautifully

--- src/test/java/com/yejianfengblue/spring/boot/jpa/JpaDummyTest.java
+++ src/test/java/com/yejianfengblue/spring/boot/jpa/JpaDummyTest.java
@@ -8,15 +8,18 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.persistence.*;
+import javax.transaction.Transactional;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * @author yejianfengblue
  */
-@DataJpaTest(showSql = false)
+@SpringBootTest
+@Transactional
 class JpaDummyTest {
 
     @Autowired
