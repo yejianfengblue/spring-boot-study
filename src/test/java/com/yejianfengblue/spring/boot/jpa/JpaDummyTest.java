20f6df722c1addf346d533893aaa0c74c1bf2c5f
Jian Feng Ye <yejianfengblue@163.com>
2019-12-06 22:07:39 +0800

    Add dependency data-jpa and h2. Add a @DataJpaTest test case to use EntityManager.persist()

---
+++ src/test/java/com/yejianfengblue/spring/boot/jpa/JpaDummyTest.java
@@ -0,0 +1,57 @@
+package com.yejianfengblue.spring.boot.jpa;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.persistence.*;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * @author yejianfengblue
+ */
+@DataJpaTest
+class JpaDummyTest {
+
+    @Autowired
+    private EntityManager entityManager;
+
+    private Logger log = LoggerFactory.getLogger(getClass());
+
+    @Entity
+    @Getter
+    @Setter
+    @ToString
+    private static class MyEntity {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String myColumn;
+    }
+
+    @Test
+    void givenNewEntityObject_whenUseEntityManagerToPersist_thenIdNotNull() {
+
+        // given
+        MyEntity myEntity = new MyEntity();
+        myEntity.setMyColumn("dummy");
+
+        log.info("myEntity before persist = {}", myEntity);
+
+        // when
+        entityManager.persist(myEntity);
+
+        log.info("myEntity after persist = {}", myEntity);
+
+        // then
+        assertNotNull(myEntity.getId());
+    }
+}
