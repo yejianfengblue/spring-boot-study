4f1f403f0be79e3926c605aa35f5055d56e5c79f
Jian Feng Ye <yejianfengblue@163.com>
2020-02-12 16:55:21 +0800

    When throw ImmediateRequeueAmqpException, message is auto requeue and redelivered

---
+++ src/test/java/com/yejianfengblue/spring/boot/rabbitmq/RequeueTest.java
@@ -0,0 +1,95 @@
+package com.yejianfengblue.spring.boot.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.ImmediateRequeueAmqpException;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Monitor the queue from RabbitMQ management UI, should see many redelivered.
+ * Monitor this app log, should see the receiver keeps receiving the same message
+ * @author yejianfengblue
+ */
+@SpringBootTest(properties = {
+        "logging.level.org.springframework.amqp.rabbit.listener.ConditionalRejectingErrorHandler = ERROR"
+})
+@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+class RequeueTest {
+
+    private static final String EXCHANGE_NAME = "spring-boot-study-rabbitmq-requeue-test-exchange";
+
+    private static final String QUEUE_NAME = "spring-boot-study-rabbitmq-requeue-test-queue";
+
+    @TestConfiguration
+    static class RabbitmqConfig {
+
+        @Bean
+        DirectExchange messageExchange() {
+            return ExchangeBuilder.directExchange(EXCHANGE_NAME)
+                    .autoDelete()
+                    .build();
+        }
+
+        /**
+         * Declare a message queue, non-auto-deleted, expires after 5min,
+         * so we can monitor this queue from management web UI and this queue will be auto deleted after expires
+         */
+        @Bean
+        Queue messageQueue() {
+            return QueueBuilder.durable(QUEUE_NAME)
+                    .expires(300_000)  // queue expires after 5min
+                    .build();
+        }
+
+        @Bean
+        Binding messageBinding() {
+            return BindingBuilder.bind(messageQueue())
+                    .to(messageExchange())
+                    .withQueueName();
+        }
+
+        @Bean
+        Receiver receiver() {
+            return new Receiver();
+        }
+
+    }
+
+    private static class Receiver {
+
+        private Logger log = LoggerFactory.getLogger(getClass());
+
+        private AtomicInteger counter = new AtomicInteger(0);
+
+        @RabbitListener(queues = QUEUE_NAME)
+        private void receive(String message) {
+
+            log.error("{} - Receive '{}', but throw exception to simulate requeue", counter.incrementAndGet(), message);
+            throw new ImmediateRequeueAmqpException("Throw exception to simulate requeue");
+        }
+
+    }
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    void givenReceiverAlwaysThrowImmediateRequeueAmqpException_whenSendMessage_thenMessageIsRequeueAndRedeliveredAndRepeat() throws InterruptedException {
+
+        rabbitTemplate.convertAndSend(EXCHANGE_NAME, QUEUE_NAME, "Say hello at " + LocalDateTime.now().toString());
+        TimeUnit.SECONDS.sleep(1);
+    }
+}
