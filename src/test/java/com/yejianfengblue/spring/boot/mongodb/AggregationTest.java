2648ab9d72b3649a03a51620276ecf96ec72b0b0
Jian Feng Ye <yejianfengblue@163.com>
2020-03-31 22:44:26 +0800

    Test to MongoTemplate.aggregate()

--- src/test/java/com/yejianfengblue/spring/boot/mongodb/AggregationTest.java
+++ src/test/java/com/yejianfengblue/spring/boot/mongodb/AggregationTest.java
@@ -18,6 +18,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.aggregation.AggregationResults;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.event.LoggingEventListener;
 import org.springframework.data.mongodb.repository.Aggregation;
@@ -29,12 +30,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.data.mongodb.core.aggregation.Aggregation.*;
+import static org.springframework.data.mongodb.core.query.Criteria.where;
 
 /**
  * Before run this test, start a clean MongoDB on localhost:27017
  */
 @SpringBootTest
-@EnableMongoRepositories(considerNestedRepositories = true, basePackageClasses = MongoEventListenerTest.class)
+@EnableMongoRepositories(considerNestedRepositories = true, basePackageClasses = AggregationTest.class)
 @EnableAutoConfiguration(exclude = {
         DataSourceAutoConfiguration.class,
         DataSourceTransactionManagerAutoConfiguration.class,
@@ -74,7 +77,7 @@ public class AggregationTest {
 
         private final int price;
 
-        private int quantity;
+        private int quantity = 1;
 
         LineItem(String name, int price, int quantity) {
 
@@ -83,7 +86,21 @@ public class AggregationTest {
         }
     }
 
-    interface OrderRepository extends MongoRepository<Order, String> {
+    @Value
+    private static class Invoice {
+
+        private final String orderId;
+
+        private final Integer taxAmount;
+
+        private final Integer netAmount;
+
+        private final Integer totalAmount;
+
+        private final List<LineItem> lineItems;
+    }
+
+    interface AggregationTestOrderRepository extends MongoRepository<Order, String> {
 
         @Aggregation("{ $group : { " +
                 "_id : $customerName, " +
@@ -93,7 +110,7 @@ public class AggregationTest {
     }
 
     @Autowired
-    private OrderRepository orderRepository;
+    private AggregationTestOrderRepository orderRepository;
 
     @Autowired
     private MongoTemplate mongoTemplate;
@@ -140,5 +157,40 @@ public class AggregationTest {
                 .isEqualTo(new OrderCountPerCustomer("Chocolate Magician Girl", 2));
 
         orderRepository.deleteAll();
+    }
+
+    @Test
+    void MongoTemplateAggregationSupportTest() {
+
+        final double taxRate = 0.1;
+
+        Order berryOrder = new Order("Berry Magician Girl", LocalDateTime.now())
+                .addItem(new LineItem("Berry Juice", 10));
+
+        berryOrder = orderRepository.save(berryOrder);
+
+        AggregationResults<Invoice> invoiceAggregationResults = mongoTemplate.aggregate(
+                newAggregation(Order.class, // TypeAggregation
+                        match(where("id").is(berryOrder.getId())),  // use class field name
+                        unwind("lineItems"),  //
+                        // project to (id, customerName, lineItems, priceTotal)
+                        project("id", "customerName", "lineItems")
+                                .andExpression("'$lineItems.price' * '$lineItems.quantity'").as("priceTotal"),  // SpEL
+                        group("id")
+                                .sum("priceTotal").as("netAmount")
+                                .addToSet("lineItems").as("lineItems"),
+                        project("lineItems", "netAmount")
+                                .and("orderId").previousOperation()
+                                .andExpression("netAmount * [0]", taxRate).as("taxAmount")
+                                .andExpression("netAmount * (1 + [0])", taxRate).as("totalAmount")
+                ),
+                Invoice.class);// output type
+
+        Invoice invoiceForBerryOrder = invoiceAggregationResults.getUniqueMappedResult();
+        assertThat(invoiceForBerryOrder.getOrderId()).isEqualTo(berryOrder.getId());
+        assertThat(invoiceForBerryOrder.getLineItems()).isEqualTo(berryOrder.getLineItems());
+        assertThat(invoiceForBerryOrder.getTaxAmount()).isEqualTo(1);
+        assertThat(invoiceForBerryOrder.getNetAmount()).isEqualTo(10);
+        assertThat(invoiceForBerryOrder.getTotalAmount()).isEqualTo(11);
     }
 }
