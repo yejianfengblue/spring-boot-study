b1e34520be9764028fdd83bff1d4f68edf62d8f5
Blue Ye <yejianfengblue@163.com>
2021-11-26 23:08:08 +0800

    Test ObjectMapper.updateValue(old, patch)

--- src/test/java/com/yejianfengblue/spring/boot/jackson/ObjectMapperTest.java
+++ src/test/java/com/yejianfengblue/spring/boot/jackson/ObjectMapperTest.java
@@ -2,19 +2,27 @@ package com.yejianfengblue.spring.boot.jackson;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Slf4j
 class ObjectMapperTest {
 
     ObjectMapper objectMapper = new ObjectMapper().enable(SerializationFeature.INDENT_OUTPUT);
@@ -89,5 +97,42 @@ class ObjectMapperTest {
                 new TypeReference<Map<String, Object>>() {}
         )).isEqualTo(
                 Map.of("color", "yellow", "type", "Bumblebee"));
+    }
+
+
+    @Getter
+    @ToString
+    @AllArgsConstructor
+    static class PojoWithReadonlyId {
+
+        private Integer id;
+
+        private String desc;
+
+        // do nothing to make id not updatable
+        private void setId(Integer id) {}
+
+        public void setDesc(String desc) {
+            this.desc = desc;
+        }
+    }
+
+    @Test
+    void objectMapperUpdateValueRespectsSetter() throws IOException {
+
+        PojoWithReadonlyId oldObject = new PojoWithReadonlyId(1, "a");
+        PojoWithReadonlyId patch = new PojoWithReadonlyId(2, "b");
+
+        PojoWithReadonlyId newObject = objectMapper.updateValue(oldObject, patch);
+        // in-place update for mutable object
+        assertThat(newObject).isSameAs(oldObject);
+        assertThat(newObject.getId()).isEqualTo(1);
+        assertThat(newObject.getDesc()).isEqualTo("b");
+
+        PojoWithReadonlyId nullPatch = new PojoWithReadonlyId(3, null);
+        PojoWithReadonlyId nullUpdatedObject = objectMapper.updateValue(oldObject, nullPatch);
+        assertThat(nullUpdatedObject).isSameAs(oldObject);
+        assertThat(nullUpdatedObject.getId()).isEqualTo(1);
+        assertThat(nullUpdatedObject.getDesc()).isNull();
     }
 }
