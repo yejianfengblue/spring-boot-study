2f4a6e6209b3b904d4de85dfca6434ca89ef6e8d
yejianfengblue <yejianfengblue@163.com>
2022-12-16 11:54:41 +0800

    Upgrade to Spring Boot 2.7.6

--- src/test/java/com/yejianfengblue/spring/boot/validation/BeanValidationTest.java
+++ src/test/java/com/yejianfengblue/spring/boot/validation/BeanValidationTest.java
@@ -6,19 +6,18 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.format.annotation.NumberFormat;
 
+import javax.validation.*;
+import javax.validation.constraints.Digits;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.lang.annotation.*;
 import java.math.BigDecimal;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
 import java.util.Optional;
 import java.util.Set;
-import javax.validation.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
